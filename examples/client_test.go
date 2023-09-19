9aaccfa24db865c280901d3d65ebe6765e3df78c
yuhuajing <18852897525@163.com>
2023-09-19 18:04:51 +0800

    update

--- examples/client_test.go
+++ examples/client_test.go
@@ -2,13 +2,92 @@ package examples
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"log"
 	"main/common/config"
 	"main/common/ethconn"
+	"main/wallet"
+	"math/big"
 	"testing"
+	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 )
+
+func Test_generateAddr(t *testing.T) {
+	priKey, _ := crypto.HexToECDSA("94498129232b8fcbd91e53c1c3cc86e9c415ac0eec5acedd7dba4aaba1226ac8")
+	publicKey := priKey.Public()
+	publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
+	fromaddress := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(fromaddress)
+}
+
+func Test_wallet_read(t *testing.T) {
+	//priKey, _ := crypto.HexToECDSA("d34772897e5d6bc952cff1094945d6d05bca81decd773f0cfb3575fbc4a73493")
+	client := ethconn.ConnBlockchain("http://localhost:8545")
+	// publicKey := priKey.Public()
+	// publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
+	// fromaddress := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	//fmt.Println(fromaddress)
+	address := common.HexToAddress("0xbB56d2FAd712F29D74241e9C45936Ac9fd049640")
+	instance, err := wallet.NewWallet(address, client)
+	if err != nil {
+		fmt.Println("error creating instance")
+		log.Fatal(err)
+	}
+
+	number, _ := instance.Gettestnum(nil)
+	fmt.Println(number)
+
+	// bal, _ := client.BalanceAt(context.Background(), common.HexToAddress(fromaddress), nil)
+	// fmt.Println(bal)
+}
+func Test_wallet_write(t *testing.T) {
+	client := ethconn.ConnBlockchain("http://localhost:8545")
+	privateKey, _ := crypto.HexToECDSA("d34772897e5d6bc952cff1094945d6d05bca81decd773f0cfb3575fbc4a73493")
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	//fmt.Println(fromaddress)
+	address := common.HexToAddress("0xbB56d2FAd712F29D74241e9C45936Ac9fd049640")
+	instance, err := wallet.NewWallet(address, client)
+	if err != nil {
+		fmt.Println("error creating instance")
+		log.Fatal(err)
+	}
+	newnumber := big.NewInt(99)
+	tx, err := instance.Settestnum(auth, newnumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("tx sent: %s\n", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
+	time.Sleep(10 * time.Second)
+	number, _ := instance.Gettestnum(nil)
+	fmt.Println("new Number: ", number)
+}
+
+//94498129232b8fcbd91e53c1c3cc86e9c415ac0eec5acedd7dba4aaba1226ac8
+//fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6b1a
 
 func Test_Scanblockdata(t *testing.T) {
 	client := ethconn.ConnBlockchain(config.EthServer)
--- wallet/README.md
+++ wallet/README.md
@@ -11,7 +11,7 @@ Wallet.sol: 实现合约，实现工厂合约的实际业务逻辑
 4. 通过  Wallet.sol 的 abi 和 bytecode 生成 对 Wallet.sol 合约的调用 golang代码 （abigen --bin=./wallet.bin --abi=./wallet.abi --pkg=wallet --out=wallet.go）
 5. 将 对合约 Wallet.sol 的调用全部转到 新的钱包地址，然后再 fallback 回 Wallet.sol 合约
 
-测试代码：
+测试读数据代码：
 ```golang
 func Test_wallet(t *testing.T) {
 	//priKey, _ := crypto.HexToECDSA("d34772897e5d6bc952cff1094945d6d05bca81decd773f0cfb3575fbc4a73493")
@@ -20,7 +20,7 @@ func Test_wallet(t *testing.T) {
 	// publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
 	// fromaddress := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
 	//fmt.Println(fromaddress)
-	address := common.HexToAddress("0xb3CC975072D317069df4Bb6C143358DF05507Be5") // 钱包地址
+	address := common.HexToAddress("0xbB56d2FAd712F29D74241e9C45936Ac9fd049640") // 钱包地址
 	instance, err := wallet.NewWallet(address, client)
 	if err != nil {
 		fmt.Println("error creating instance")
@@ -32,5 +32,49 @@ func Test_wallet(t *testing.T) {
 
 	// bal, _ := client.BalanceAt(context.Background(), common.HexToAddress(fromaddress), nil)
 	// fmt.Println(bal)
+}
+```
+
+测试写数据：
+```golang
+func Test_wallet_write(t *testing.T) {
+	client := ethconn.ConnBlockchain("http://localhost:8545")
+	privateKey, _ := crypto.HexToECDSA("d34772897e5d6bc952cff1094945d6d05bca81decd773f0cfb3575fbc4a73493")
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)     // in wei
+	auth.GasLimit = uint64(300000) // in units
+	auth.GasPrice = gasPrice
+
+	//fmt.Println(fromaddress)
+	address := common.HexToAddress("0xbB56d2FAd712F29D74241e9C45936Ac9fd049640")
+	instance, err := wallet.NewWallet(address, client)
+	if err != nil {
+		fmt.Println("error creating instance")
+		log.Fatal(err)
+	}
+	newnumber := big.NewInt(99)
+	tx, err := instance.Settestnum(auth, newnumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("tx sent: %s\n", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
+	time.Sleep(10 * time.Second)
+	number, _ := instance.Gettestnum(nil)
+	fmt.Println("new Number: ", number)
 }
 ```
